9350e842a75dd4580d8a5ed8e602ec61b04733e1
David Corbett <dcorbett@navinet.net>
2014-03-10 10:40:57 -0400

    Add testing of an entire directory

--- poc.py
+++ poc.py
@@ -1,59 +1,61 @@
 #!/usr/bin/env python
 
-from applitools.eyes import Eyes
-from applitools._match_window_task import MatchWindowTask
-from collections import defaultdict
-from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.remote.webdriver import WebDriver
+import collections
+import os
+import os.path
+
+import applitools.eyes
+from applitools import _match_window_task
+from selenium.common import exceptions
+from selenium.webdriver.remote import webdriver
 
 
-Eyes.api_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+applitools.eyes.Eyes.api_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 
-def match_window(eyes, tag, path):
+def match_window(eyes, path):
     if not eyes._running_session:
         eyes._start_session()
-        eyes._match_window_task = MatchWindowTask(eyes, eyes._agent_connector,
-                                                  eyes._running_session, None,
-                                                  eyes.match_timeout)
+        eyes._match_window_task = _match_window_task.MatchWindowTask(
+            eyes, eyes._agent_connector, eyes._running_session, None,
+            eyes.match_timeout)
     with open(path, 'rb') as f:
         screenshot64 = f.read().encode('base64')
     data = {'appOutput': {'title': '', 'screenshot64': screenshot64},
             'userInputs': [],
-            'tag': tag,
+            'tag': None,
             'ignoreMismatch': False}
     eyes._match_window_task._agent_connector.match_window(
         eyes._match_window_task._running_session, data)
 
 
-def test(tags, paths):
-    class FakeWebDriver(WebDriver):
+def test(path):
+    class FakeWebDriver(webdriver.WebDriver):
         def __init__(self):
             pass
 
         def get_window_size(self):
-            return defaultdict(int)
+            return collections.defaultdict(int)
 
         def execute_script(self, script):
-            raise WebDriverException
+            raise exceptions.WebDriverException
 
     try:
-        eyes = Eyes()
-        eyes.open(driver=FakeWebDriver(), app_name='c', test_name='baseline')
-        for tag, path in zip(tags, paths):
-            match_window(eyes, tag, path)
+        paths = [os.path.join(path, f) for f in os.listdir(path)]
+    except OSError:
+        paths = [path]
+    try:
+        eyes = applitools.eyes.Eyes()
+        eyes.open(driver=FakeWebDriver(), app_name='p2', test_name='t')
+        for path in paths:
+            match_window(eyes, path)
         eyes.close()
     finally:
         eyes.abort_if_not_closed()
 
 
 def main():
-    test(['Screen 1', 'Screen 2'],
-         ['C:\\Users\\DCorbett\\Desktop\\Applitools\\New\\'
-          'selenium-screenshot-1.png',
-          'C:\\Users\\DCorbett\\Desktop\\Applitools\\New\\'
-          'selenium-screenshot-2.png'])
+    test(r'C:\Users\DCorbett\Desktop\Applitools\Baseline')
 
 
 if __name__ == '__main__':
