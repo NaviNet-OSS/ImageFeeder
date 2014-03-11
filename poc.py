7a5e41c222f1bf748aab389808cd8a42b07e091f
David Corbett <dcorbett@navinet.net>
2014-03-11 14:31:54 -0400

    Add docstrings and simplify the fake web driver

--- poc.py
+++ poc.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import collections
-import os
 import os.path
 
-import applitools.eyes
 from applitools import _match_window_task
 from applitools import errors
-from selenium.common import exceptions
+import applitools.eyes
 from selenium.webdriver.remote import webdriver
 
 
+API_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 APP_NAME = 'app'
 TEST_NAME = 'test'
 EYES_SERVER = applitools.eyes.Eyes.DEFAULT_EYES_SERVER
 
 
 def match_window(eyes, path):
+    """Sends an image to Applitools for matching.
+
+    Args:
+        eyes: An open Eyes instance.
+        path: The path of an image. The file name is used as the tag.
+    """
     if not eyes._running_session:
         eyes._start_session()
         eyes._match_window_task = _match_window_task.MatchWindowTask(
@@ -34,15 +38,45 @@ def match_window(eyes, path):
 
 
 def test(path, overwrite_baseline=False):
-    class FakeWebDriver(webdriver.WebDriver):
+    """Matches images against the baseline.
+
+    Matches a single image or a directory of images against the
+    baseline in Applitools. If there is no baseline for this app, it
+    creates one. If the test did not pass, it prints Applitools's error
+    output.
+
+    Args:
+        path: The path of either an image or a directory of images.
+        overwrite_baseline: Whether to overwrite the baseline with the
+            results of this test.
+    """
+    class _FakeWebDriver(webdriver.WebDriver):
+        """A fake web driver.
+
+        Applitools currently requires Selenium to work, so we need a
+        web driver to trick it into working.
+
+        Attributes:
+            capabilities: A dictionary of capability names to booleans.
+                The only one that matters is 'takesScreenshot', which
+                must be True, or else Applitools will try (and fail) to
+                take screenshots itself.
+        """
+
         def __init__(self):
+            """Initializes capabilities."""
             self.capabilities = {'takesScreenshot': True}
 
-        def get_window_size(self):
-            return collections.defaultdict(int)
+        def execute_script(self, script, params=None):
+            """Returns a fake viewport dimension.
 
-        def execute_script(self, script):
-            raise exceptions.WebDriverException
+            The only scripts that must be mocked are those that get the
+            width and height of the viewport.
+
+            Returns:
+                A valid viewport dimension.
+            """
+            return 0
 
     try:
         paths = [os.path.join(path, f) for f in os.listdir(path)]
@@ -51,7 +85,7 @@ def test(path, overwrite_baseline=False):
     try:
         eyes = applitools.eyes.Eyes(EYES_SERVER)
         eyes.save_failed_tests = overwrite_baseline
-        eyes.open(FakeWebDriver(), APP_NAME, TEST_NAME)
+        eyes.open(_FakeWebDriver(), APP_NAME, TEST_NAME)
         for path in paths:
             match_window(eyes, path)
         eyes.close()
@@ -62,8 +96,7 @@ def test(path, overwrite_baseline=False):
 
 
 def main():
-    applitools.eyes.Eyes.api_key = (
-        'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+    applitools.eyes.Eyes.api_key = API_KEY
     test(r'C:\Users\DCorbett\Desktop\Applitools\Baseline', True)
     test(r'C:\Users\DCorbett\Desktop\Applitools\New')
 
