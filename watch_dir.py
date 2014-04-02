a4e38b06aca602534871f0ad12f2552f15de73d5
David Corbett <dcorbett@navinet.net>
2014-04-02 16:51:28 -0400

    Limit the number of concurrent tests
    A test finishes when a file name 'done' is created.

--- watch_dir.py
+++ watch_dir.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import errno
 import os
 import Queue
 import sys
@@ -15,38 +16,63 @@ import eyes_wrapper
 
 
 _QUEUES = []
+DONE_BASE_NAME = 'done'
+PROCESSING_DIR_NAME = 'processing'
+
+# The Applitools Eyes Team License limits the number of concurrent
+# tests to n + 1, where n is the number of team members. (We have five
+# members.) However, Applitools does not enforce this limit; until they
+# do, we are free to test as much as we want.
+_MAX_CONCURRENT_TESTS = 6
+_CONCURRENT_TEST_QUEUE = Queue.Queue(_MAX_CONCURRENT_TESTS)
 
 # See http://robotframework.googlecode.com/hg/doc/userguide/RobotFrameworkUserGuide.html#test-library-scope
 ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
 
 
 class WindowMatchingEventHandler(events.FileSystemEventHandler):
-    """Event handler that sends new images to Applitools.
+    """Event handler that sends new files to Applitools.
     """
 
-    def __init__(self, eyes):
+    def __init__(self, eyes, queue):
         """Initializes the event handler.
 
         Args:
             eyes: An open Eyes instance to send images through.
+            queue: A Queue to make non-empty when it is time to stop
+                watching.
         """
         self._eyes = eyes
+        self._queue = queue
 
     def on_created(self, event):
-        """Sends a new image to Applitools.
+        """Sends a new file to Applitools.
 
-        Does not send directories. Silently ignores errors from sending
-        non-image files.
+        Does not send directories. Moves files to a processing
+        subdirectory. Silently ignores errors from sending non-image
+        files. Stops watching when a file called 'done' is created.
 
         Args:
             event: The file system event.
         """
         if os.path.isfile(event.src_path):
-            try:
-                eyes_wrapper.match_window(self._eyes, event.src_path)
-            except exceptions.HTTPError:
-                # The file wasn't a valid image.
-                pass
+            print(str(event.src_path))
+            if os.path.basename(event.src_path) == DONE_BASE_NAME:
+                # Stop watching the path
+                self._queue.put(None)
+                # Allow another path to be watched
+                _CONCURRENT_TEST_QUEUE.get()
+                _CONCURRENT_TEST_QUEUE.task_done()
+            else:
+                try:
+                    new_path = event.src_path
+                    # head, tail = os.path.split(event.src_path)
+                    # new_path = os.path.join(head, PROCESSING_DIR_NAME, tail)
+                    # os.rename(event.src_path, new_path)
+                    eyes_wrapper.match_window(self._eyes, new_path)
+                except exceptions.HTTPError:
+                    # The file wasn't a valid image.
+                    pass
 
 
 def _send_new_files(path, eyes, queue):
@@ -60,7 +86,7 @@ def _send_new_files(path, eyes, queue):
         queue: A Queue. When it becomes non-empty, it is time to stop
             watching for new files.
     """
-    event_handler = WindowMatchingEventHandler(eyes)
+    event_handler = WindowMatchingEventHandler(eyes, queue)
     observer = observers.Observer()
     observer.schedule(event_handler, path)
     observer.start()
@@ -72,11 +98,18 @@ def _send_new_files(path, eyes, queue):
 def watch_path(path):
     """Sends new files to Applitools in another thread.
 
-    Watches a directory for files to send.
+    Watches a directory for files to send. Moves new files to a
+    processing subdirectory.
 
     Args:
         path: The name of the directory to watch.
     """
+    _CONCURRENT_TEST_QUEUE.put(None)
+    try:
+        os.makedirs(os.path.join(path, PROCESSING_DIR_NAME))
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
     queue = Queue.Queue()
     _QUEUES.append(queue)
     threading.Thread(target=lambda: eyes_wrapper.run_eyes(
